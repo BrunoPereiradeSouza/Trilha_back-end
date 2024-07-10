f8b29d474f5d500044faf8a76abeedb97e7e0fc1
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-07-09 23:01:34 -0300

    feat: Product CRUD

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'products'
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name='products')
+    path('', views.ProductListView, name='index'),
+    path('product/update/<int:id>/', views.ProductUpdateView, name='update'),
+    path('product/delete/<int:id>', views.ProductDeleteView, name='deletar')
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,36 @@
-from django.views.generic import ListView
-from django.views.generic import UpdateView
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
+from .forms import ProductForm
 
 
-class ProductListView(ListView):
-    model = Product
-    template_name = 'products/index.html'
-    context_object_name = 'products'
-    paginate_by = 5
-    ordering = ['-id']
+def ProductListView(request):
+    products = Product.objects.all()
+    return render(request, 'products/index.html',
+                  context={'products': products}
+                  )
+
+
+def ProductUpdateView(request, id):
+    product = get_object_or_404(Product, id=id)
+
+    if request.method == 'GET':
+        form = ProductForm(instance=product)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    return render(request, 'products/form.html',
+                  context={
+                      'form': form
+                  }
+                  )
+
+
+def ProductDeleteView(request, id):
+    product = get_object_or_404(Product, id=id)
+    product.delete()
+    return redirect('index')
