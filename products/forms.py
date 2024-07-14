a0f15a7598fda4e2f382401c24642b76b4bdfe13
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-07-14 20:31:21 -0300

    feat: add bootstrap to product create form

--- products/forms.py
+++ products/forms.py
@@ -9,3 +9,11 @@ class ProductForm(forms.ModelForm):
                   'category', 'quantity_stocked',
                   'cover',
                   ]
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'quantity_stocked': forms.NumberInput(attrs={'class': 'form-control'}),
+            'cover': forms.FileInput(attrs={'class': 'form-control'}),
+        }
