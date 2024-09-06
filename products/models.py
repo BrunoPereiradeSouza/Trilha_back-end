b056c8421c8b07bd057f4e909f3c5efe908c56e4
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-09-06 11:12:26 -0300

    fix: correct product total value in cart

--- products/models.py
+++ products/models.py
@@ -24,6 +24,7 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
 class Sale(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     client = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -36,7 +37,8 @@ class ItemCart(models.Model):
     def __str__(self):
         return self.product.name
 
-    def get_total_value(self):
+    @property
+    def total_value(self):
         return self.quantity * self.product.price
 
 
@@ -46,7 +48,7 @@ class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Carrinho de {self.user.username}'
-    
+        return f"Carrinho de {self.user.username}"
+
     def get_cart_total_value(self):
         return sum(item.get_total_value() for item in self.itens.all())
